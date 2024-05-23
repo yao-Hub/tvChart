bb46e63da595ac5f4da16141e628189e125c29e7
项目组-前端-姚泽宇 <244869028@qq.com>
2024-05-23 14:19:24 +0800

    feat: 打包代码混淆

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,17 @@ import vue from '@vitejs/plugin-vue'
 import { resolve } from "path"
 import Components from 'unplugin-vue-components/vite';
 import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
+import { visualizer } from "rollup-plugin-visualizer";
 
 export default defineConfig((mode: ConfigEnv) => {
   const env = loadEnv(mode.mode, process.cwd());
-  const root = process.cwd();
-
+  // const root = process.cwd();
   return {
     base: '/',
     // root,
+    build: {
+      minify: 'terser'
+    },
     plugins: [
       vue(),
       Components({
@@ -20,6 +23,12 @@ export default defineConfig((mode: ConfigEnv) => {
           }),
         ],
       }),
+      visualizer({
+        open: false,  //注意这里要设置为true，否则无效
+        filename: "stats.html", //分析图生成的文件名
+        gzipSize: true, // 收集 gzip 大小并将其显示
+        brotliSize: true, // 收集 brotli 大小并将其显示
+      })
     ],
     resolve: {
       alias: {
