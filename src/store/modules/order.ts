0985f69c59a80bf8e3adf833fd8a6bf32478315c
项目组-前端-姚泽宇 <244869028@qq.com>
2024-06-18 15:42:06 +0800

    feat: 当前账户是否有交易权限判断、止损止盈各个单位之间的联动计算

--- src/store/modules/order.ts
+++ src/store/modules/order.ts
@@ -2,24 +2,17 @@ import { defineStore } from 'pinia';
 import { useDialog } from './dialog';
 import { useChartAction } from './chartAction';
 import { useUser } from './user';
-import { Line } from '#/chart/index';
+import * as types from '#/chart/index';
+import { Modal } from 'ant-design-vue';
 
 const dialogStore = useDialog();
 const chartActionStore = useChartAction();
 const userStore = useUser();
 
-interface Quote {
-  ask: number
-  bid: number
-  ctm: number
-  ctm_ms: number
-  server: string
-  symbol: string
-}
 interface State {
-  currentQuote: Quote | null
+  currentQuote: types.Quote | null
   currentSymbol: string
-  currentKline: Line | null
+  currentKline: types.Line | null
   refreshOrderArea: boolean
   polling: boolean // 是否轮询个人信息和持仓单
 }
@@ -40,6 +33,13 @@ export const useOrder = defineStore('order', {
       if (!userStore.getToken()) {
         dialogStore.showLoginDialog();
         chartActionStore.setCacheAction('createOrder');
+        return;
+      }
+      if (userStore.loginInfo?.trade_rights !== 1) {
+        Modal.error({
+          title: '无权限',
+          content: '当前账户禁止交易',
+        });
         return;
       }
       dialogStore.showOrderDialog();
