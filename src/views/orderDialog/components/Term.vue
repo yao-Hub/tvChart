027ddd958a6f5d2fdf5b8834b1e42e047fbc5424
项目组-前端-姚泽宇 <244869028@qq.com>
2024-09-13 16:38:13 +0800

    feat: 时间期限样式更改

--- src/views/orderDialog/components/Term.vue
+++ src/views/orderDialog/components/Term.vue
@@ -1,57 +1,34 @@
 <!-- 期限 -->
 <template>
-  <a-flex gap="4">
-    <a-form-item no-style>
-      <a-date-picker
-        style="width: 100%"
-        :allowClear="false"
-        v-model:value="day"
-        :format="dateFormat"
-        :disabled-date="disabledDate"
-      />
-    </a-form-item>
-    <a-form-item no-style>
-      <a-time-picker
-        style="width: 100%"
-        v-model:value="time"
-        :allowClear="false"
-        :format="timeFormat"
-        :showNow="false"
-      />
-    </a-form-item>
-  </a-flex>
+  <a-date-picker
+    style="width: 100%"
+    show-time
+    placeholder="Select Time"
+    :format="dateFormat"
+    :disabled-date="disabledDate"
+    v-model:value="date"
+  />
 </template>
 
 <script setup lang="ts">
-import { ref, computed, onMounted, watchEffect } from "vue";
+import { ref, computed, watchEffect } from "vue";
 
 import dayjs, { Dayjs } from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 dayjs.extend(relativeTime);
 
-const dateFormat = "DD/MM/YYYY";
-const timeFormat = "HH:mm";
+const dateFormat = "DD/MM/YY HH:mm";
 
-const day = ref<Dayjs>(dayjs());
-const time = ref<Dayjs>(dayjs().add(12, "minute"));
+const date = ref<Dayjs>(dayjs().add(12, "minute"));
 
 // Can not select days before today
 const disabledDate = (current: Dayjs) => {
   return current && current < dayjs().startOf("day");
 };
 
-onMounted(() => {
-  time.value = dayjs().add(12, "minute");
-  day.value = dayjs();
-});
-
 const dayTimeStamp = computed(() => {
-  const daystr = dayjs(day.value).format(dateFormat);
-  const timestr = dayjs(time.value).format(timeFormat);
-  const timeStamp = dayjs(
-    `${daystr} ${timestr}`,
-    `${dateFormat} ${timeFormat}`
-  ).unix();
+  const datestr = dayjs(date.value).format(dateFormat);
+  const timeStamp = dayjs(datestr, dateFormat).unix();
   return timeStamp;
 });
 
