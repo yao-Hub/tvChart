5f8355069769d9f905c9030eff50bd28b08b0304
项目组-前端-姚泽宇 <244869028@qq.com>
2024-10-16 15:32:31 +0800

    build: scss

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ import "./assets/icons/iconfont/iconfont.css";
 
 import "element-plus/dist/index.css";
 
-import "./styles/element/dark/css-vars.scss";
+// import "./styles/element/dark/css-vars.scss";
 import "./styles/element/dark/index.scss";
 
 import "./styles/element/index.scss";
