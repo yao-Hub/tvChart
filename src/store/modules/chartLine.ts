3b55877661b49b608d3886d91a45b05a42d19ac9
yaozeyu <244869028@qq.com>
2024-12-30 11:18:07 +0800

    fix: 修复k线的显示

--- src/store/modules/chartLine.ts
+++ src/store/modules/chartLine.ts
@@ -1,5 +1,6 @@
 import { useOrder } from "@/store/modules/order";
 import * as types from "@/types/chart/index";
+import { sortBy } from "lodash";
 import { defineStore } from "pinia";
 import { useSocket } from "./socket";
 interface ISubSCribed {
@@ -26,12 +27,40 @@ export const useChartLine = defineStore("chartLine", {
     initSubLineAndQuote() {
       const socketStore = useSocket();
       const orderStore = useOrder();
+
+      const setHigh = (bid: number, high?: number) => {
+        if (!high) {
+          return bid;
+        }
+        if (high < bid) {
+          return bid;
+        }
+        if (high) {
+          return high;
+        }
+        return bid;
+      };
+      const setLow = (bid: number, low?: number) => {
+        if (!low) {
+          return bid;
+        }
+        if (low > bid) {
+          return bid;
+        }
+        if (low) {
+          return low;
+        }
+        return bid;
+      };
+
       socketStore.subQuote((d) => {
         const oldQuote = orderStore.currentQuotes[d.symbol];
         const result = {
           ...oldQuote,
           ...d,
           close: d.bid,
+          high: setHigh(d.bid, oldQuote.high),
+          low: setHigh(d.bid, oldQuote.low),
         };
         orderStore.currentQuotes[d.symbol] = result;
         for (const UID in this.subscribed) {
@@ -41,23 +70,18 @@ export const useChartLine = defineStore("chartLine", {
           const bar = this.newbar[subscriberUID];
           if (bar && d.symbol === symbol) {
             const bartime = bar.time;
-            let high = bar.high;
-            let low = bar.low;
+            const high = setHigh(d.bid, bar.high);
+            const low = setLow(d.bid, bar.low);
             const volume = bar.volume || 0;
-            if (!high || +high < +d.bid) {
-              high = d.bid;
-            }
-            if (!low || +low > +d.bid) {
-              low = d.bid;
-            }
             let result = {
-              ...d,
-              ...bar,
+              open: bar.open,
+              ask: d.ask,
+              bid: d.bid,
               close: +d.bid,
               high: +high,
               low: +low,
               volume: volume + 1,
-              time: bartime,
+              time: d.ctm * 1000,
             };
             if (!bartime || bartime < d.ctm * 1000) {
               result.time = d.ctm * 1000;
@@ -80,28 +104,28 @@ export const useChartLine = defineStore("chartLine", {
             +item.resolution == d.period_type
           ) {
             const nowbarTime = nowBar.time;
-            for (let i = 0; i < d.klines.length; i++) {
-              const line = d.klines[i];
-              const { close, open, volume, high, low, ctm } = line;
-              const dTime = ctm * 1000;
-              let result = {
-                close,
-                open,
-                volume,
-                high,
-                low,
-                time: dTime,
-              };
-              if (!nowbarTime || nowbarTime <= dTime) {
-                this.newbar[subscriberUID] = { ...result };
-              }
-              orderStore.currentQuotes[d.symbol]["close"] = close;
-              orderStore.currentQuotes[d.symbol]["open"] = open;
-              orderStore.currentQuotes[d.symbol]["high"] = high;
-              orderStore.currentQuotes[d.symbol]["low"] = low;
-              orderStore.currentQuotes[d.symbol]["volume"] = volume;
-              this.subscribed[UID].onRealtimeCallback(result);
+            const lines = sortBy(d.klines, ["ctm"]);
+            const line = lines.slice(-1)[0];
+            const { close, open, volume, high, low, ctm } = line;
+            const dTime = ctm * 1000;
+            const result = {
+              close,
+              open,
+              volume,
+              high,
+              low,
+              time: nowbarTime,
+            };
+            if (!nowbarTime || nowbarTime < dTime) {
+              result.time = dTime;
             }
+            this.newbar[subscriberUID] = { ...result };
+            this.subscribed[UID].onRealtimeCallback(result);
+            orderStore.currentQuotes[d.symbol]["close"] = close;
+            orderStore.currentQuotes[d.symbol]["open"] = open;
+            orderStore.currentQuotes[d.symbol]["high"] = high;
+            orderStore.currentQuotes[d.symbol]["low"] = low;
+            orderStore.currentQuotes[d.symbol]["volume"] = volume;
           }
         }
       });
