4ecb293f39960c32685cf363c6aa64cc5f9b94e8
yaozeyu <244869028@qq.com>
2025-02-17 11:30:11 +0800

    perf: 优化提升版本号逻辑

--- scripts/update-version.ts
+++ scripts/update-version.ts
@@ -2,6 +2,15 @@ import { readFileSync, writeFileSync } from "fs";
 import inquirer from "inquirer";
 import simpleGit, { SimpleGit } from "simple-git";
 
+// 绿色 ANSI 转义序列
+const greenColor = "\x1b[32m";
+// 红色 ANSI 转义序列
+const redColor = "\x1b[31m";
+// 黄色 ANSI 转义序列
+const yellowColor = "\x1b[33m";
+// 重置颜色的 ANSI 转义序列
+const resetColor = "\x1b[0m";
+
 // 从远程仓库获取最新标签版本号
 async function getRemoteLatestTag(
   git: SimpleGit
@@ -22,7 +31,7 @@ function shouldUpdate(
   remoteVersion: string | null | undefined
 ): boolean {
   if (!remoteVersion) return true;
-  const localParts = localVersion.split(".").map(Number);
+  const localParts = localVersion.replace("v", "").split(".").map(Number);
   const remoteParts = remoteVersion.replace("v", "").split(".").map(Number);
   for (let i = 0; i < 3; i++) {
     if (localParts[i] > remoteParts[i]) {
@@ -32,16 +41,50 @@ function shouldUpdate(
   return true;
 }
 
-// 检查更新类型是否符合规则
-function getUpdateVersion(
+// 是否可以更新
+function canUpdate(
+  inputVersion: string,
+  remoteVersion: string | null | undefined
+): boolean {
+  if (!remoteVersion) return true;
+  const inputParts = inputVersion.replace("v", "").split(".").map(Number);
+  const remoteParts = remoteVersion.replace("v", "").split(".").map(Number);
+  for (let i = 0; i < 3; i++) {
+    if (inputParts[i] > remoteParts[i]) {
+      return true;
+    }
+  }
+  return false;
+}
+
+// 获取版本
+async function getUpdateVersion(
   localVersion: string,
-  remoteVersion: string | null | undefined,
-  updateType: "patch" | "minor" | "major"
-): string {
+  updateType: "patch" | "minor" | "major" | "custom",
+  remoteVersion: string | null | undefined
+): Promise<string> {
+  if (updateType === "custom") {
+    const answers = await inquirer.prompt([
+      {
+        type: "input",
+        name: "customVersion",
+        message: "请输入自定义版本号:",
+        validate: (input) => {
+          const versionRegex = /^\d+(\.\d+){0,2}$/;
+          if (!versionRegex.test(input)) {
+            return `${redColor}×${resetColor} 请输入有效的版本号，格式如  x.y.z`;
+          }
+          const ifCanUpdate = canUpdate(input, remoteVersion);
+          if (!ifCanUpdate) {
+            return `${redColor}×${resetColor} 版本号小于远程版本号`;
+          }
+          return true;
+        },
+      },
+    ]);
+    return answers.customVersion;
+  }
   const localParts = localVersion.split(".").map(Number);
-  const remoteParts = remoteVersion
-    ? remoteVersion.split(".").map(Number)
-    : localParts;
 
   const newParts = [...localParts];
   if (updateType === "patch") {
@@ -65,11 +108,20 @@ function updatePackageJsonVersion(version: string) {
   packageJson.version = version;
   const newPackageJsonContent = JSON.stringify(packageJson, null, 2);
   writeFileSync(packageJsonPath, newPackageJsonContent);
-  // 绿色 ANSI 转义序列
-  const greenColor = "\x1b[32m";
-  // 重置颜色的 ANSI 转义序列
-  const resetColor = "\x1b[0m";
-  console.log(`${greenColor}√${resetColor} 版本号已更新为 ${version}`);
+  console.log(`${greenColor}√${resetColor} 本地版本号已更新为 ${version}`);
+}
+
+async function updateRemoteTag(version: string) {
+  try {
+    const git: SimpleGit = simpleGit();
+    // 打标签
+    await git.tag([`v${version}`]);
+    // 推送标签到远程仓库
+    await git.push(["origin", `v${version}`]);
+    console.log(`****版本号 v${version} 已同步到远程仓库****`);
+  } catch (error) {
+    console.error("****同步版本号到远程仓库时出错:", error);
+  }
 }
 
 // 主函数
@@ -77,20 +129,38 @@ async function main() {
   const packageJson = require("../package.json");
   const localVersion = packageJson.version;
   const git: SimpleGit = simpleGit();
+  console.log("********版本号更新********");
   const remoteVersion = await getRemoteLatestTag(git);
   console.log("本地版本号: ", localVersion);
   console.log("远程版本号: ", remoteVersion);
 
+  if (remoteVersion == null) {
+    console.log(`${yellowColor}√${resetColor} 远程版本为空，同步本地版本`);
+    updateRemoteTag(localVersion);
+    return;
+  }
+
   if (shouldUpdate(localVersion, remoteVersion)) {
-    console.log("****需要更新版本号****");
+    console.log(`${redColor}!${resetColor} 需要更新版本号`);
+    console.log(
+      `${yellowColor}?${resetColor} Patch:缺陷修复，代码的功能没有发生变化，例如，从 1.0.0 升级到 1.0.1。\n`
+    );
+    console.log(
+      `${yellowColor}?${resetColor} Minor: 向后兼容的方式添加了新功能时，旧版本的代码在新版本中仍然可以正常运行，同时还能使用新添加的功能，例如，从 1.0.x 升级到 1.1.x \n`
+    );
+    console.log(
+      `${yellowColor}?${resetColor} Major: 不兼容的 API 修改时，之前使用旧版本 API 的代码在升级到新版本后可能无法正常工作，例如，从 1.x.x 升级到 2.x.x。\n`
+    );
+
     const versionOptions = [
       { name: "补丁版本更新 (Patch)", value: "patch" },
       { name: "小版本更新 (Minor)", value: "minor" },
       { name: "大版本更新 (Major)", value: "major" },
+      { name: "自定义版本号 (custom)", value: "custom" },
     ];
 
     const answers = await inquirer.prompt<{
-      versionType: "patch" | "minor" | "major";
+      versionType: "patch" | "minor" | "major" | "custom";
     }>([
       {
         type: "list",
@@ -101,12 +171,13 @@ async function main() {
     ]);
 
     const updateType = answers.versionType;
-    const updateVersion = getUpdateVersion(
+    const updateVersion = await getUpdateVersion(
       localVersion,
-      remoteVersion,
-      updateType
+      updateType,
+      remoteVersion
     );
     updatePackageJsonVersion(updateVersion);
+    updateRemoteTag(updateVersion);
   } else {
     console.log("****本地版本号已是最新，无需更新。****");
   }
