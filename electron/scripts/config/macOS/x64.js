42f3096606388ee35ea02b254369c6d2ac9bf5de
RolinOS <lkrolin@163.com>
2025-03-18 16:51:33 +0800

    feat: mac打包配置

--- electron/scripts/config/macOS/x64.js
+++ electron/scripts/config/macOS/x64.js
@@ -20,6 +20,7 @@ module.exports.default = {
     ],
     artifactName: "${productName}-Mac-${version}-x64.${ext}",
     icon: "public/logo_512.png",
+    identity: null,  // 禁用签名
   },
   electronVersion: "34.0.2",
 };
