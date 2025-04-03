20ca01486fc5b17e0e8760b4c8eef8c17c087ec4
DESKTOP-8E3OU39\l <244869028@qq.com>
2025-04-03 16:00:03 +0800

    feat:版本更新频率

--- src/store/modules/version.ts
+++ src/store/modules/version.ts
@@ -10,6 +10,10 @@ interface IState {
   deviceId: string;
   versionInfo: IReqVersion | null;
 }
+interface IGetUpdate {
+  status: number[];
+  ifCheckFrequency?: boolean;
+}
 export const useVersion = defineStore("version", {
   state: (): IState => ({
     deviceId: "",
@@ -25,27 +29,52 @@ export const useVersion = defineStore("version", {
       return this.deviceId;
     },
     // 获取更新
-    async getUpdate(showStatusList: number[] = [-1, 0, 1, 2]) {
+    async getUpdate(params?: IGetUpdate) {
+      let status = [-1, 0, 1, 2];
+      let ifCheckFrequency = false;
+
+      if (params) {
+        status = params.status;
+        ifCheckFrequency = params.ifCheckFrequency || false;
+      }
+
       if (!process.env.IF_ELECTRON) {
         return;
       }
-      let type = -1;
-      const res = await versionQuery();
-      if (res.data) {
-        this.versionInfo = res.data;
-        type = res.data.updateType;
-      }
-      // useDialog().openDialog("updateVersionVisible");
-      // return;
-      if (showStatusList.includes(type)) {
-        if (type === -1) {
-          const t = i18n.global.t;
-          ElNotification.warning(
-            t("tip.failed", { type: t("update.getUpdate") })
-          );
+      const upDateStamp = localStorage.getItem("upDateStamp");
+      if (upDateStamp && ifCheckFrequency) {
+        const currentTime = new Date().getTime();
+        const startTime = JSON.parse(upDateStamp);
+        const oneDayInMilliseconds = 24 * 60 * 60 * 1000;
+        const timeDifference = currentTime - startTime;
+        if (timeDifference < oneDayInMilliseconds) {
           return;
         }
-        useDialog().openDialog("updateVersionVisible");
+      }
+
+      let type = 0;
+      try {
+        const res = await versionQuery();
+        if (res.data) {
+          this.versionInfo = res.data;
+          type = res.data.updateType;
+          if (res.data.tipsFrequency === 1) {
+            localStorage.setItem(
+              "upDateStamp",
+              new Date().getTime().toString()
+            );
+          } else {
+            localStorage.removeItem("upDateStamp");
+          }
+        }
+        if (status.includes(type)) {
+          useDialog().openDialog("updateVersionVisible");
+        }
+      } catch (error) {
+        const t = i18n.global.t;
+        ElNotification.warning(
+          t("tip.failed", { type: t("update.getUpdate") })
+        );
       }
     },
   },
